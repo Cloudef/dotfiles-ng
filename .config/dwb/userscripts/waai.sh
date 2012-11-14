955a4e1ff6dd899f38f695c61675cb5b9054ffd2
Jari Vetoniemi <mailroxas@gmail.com>
2012-11-15 00:36:15 +0200

    curl might take longer and loliclip timeouts fix by storing url in variable

--- .config/dwb/userscripts/waai.sh
+++ .config/dwb/userscripts/waai.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # dwb: Control w
-curl -s "http://waa.ai/api.php?url=$(echo $DWB_URI | perl -MURI::Escape -lne 'print uri_escape($_)')" | loliclip -c
+url="$(curl -s "http://waa.ai/api.php?url=$(echo $DWB_URI | perl -MURI::Escape -lne 'print uri_escape($_)')")"
+echo "$url" | loliclip -c
