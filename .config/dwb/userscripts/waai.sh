0a1965c5aec285ba1cea235cde29c1b26161d994
Jari Vetoniemi <mailroxas@gmail.com>
2013-04-22 18:34:09 +0300

    Update waai.sh work with new loliclip

--- .config/dwb/userscripts/waai.sh
+++ .config/dwb/userscripts/waai.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # dwb: Control w
 url="$(curl -s "http://waa.ai/api.php?url=$(echo $DWB_URI | perl -MURI::Escape -lne 'print uri_escape($_)')")"
-echo "$url" | loliclip -c
+echo "$url" | loliclip -ci
