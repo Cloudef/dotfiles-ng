a7ad3c69cdead9415701a2b2b8c9cfd1cfad0d1e
Jari Vetoniemi <mailroxas@gmail.com>
2013-11-04 17:39:15 +0200

    Change all scripts to use xcmenu instead of loliclip

--- .config/dwb/userscripts/waai.sh
+++ .config/dwb/userscripts/waai.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # dwb: Control w
 url="$(curl -s "http://api.waa.ai/?url=$(echo $DWB_URI | perl -MURI::Escape -lne 'print uri_escape($_)')")"
-echo "$url" | loliclip -ci
+echo "$url" | xcmenu -ci
